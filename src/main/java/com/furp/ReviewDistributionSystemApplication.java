688cc67131e9d3ffd3556e8303934814cfdf3cf3
touse34 <ssyyy0524@163.com>
2025-06-24 18:32:38 +0800

    initial commit

---
+++ src/main/java/com/furp/ReviewDistributionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.furp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewDistributionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewDistributionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/furp/ReviewDistributionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewDistributionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
