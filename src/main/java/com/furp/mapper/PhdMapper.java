624834faafdc7f6ae1f63563965cd2956ac696dc
touse34 <ssyyy0524@163.com>
2025-06-28 23:49:42 +0800

    new function

---
+++ src/main/java/com/furp/mapper/PhdMapper.java
@@ -0,0 +1,8 @@
+package com.furp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface PhdMapper extends BaseMapper<PhdMapper> {
+
+
+}
--- src/main/java/com/furp/service/impl/PhdServiceImpl.java
+++ src/main/java/com/furp/service/impl/PhdServiceImpl.java
@@ -1,4 +1,9 @@
 package com.furp.service.impl;
 
 public class PhdServiceImpl {
+
+    /** 获取所有状态为pending的学生
+     *
+     *
+     * */
 }
