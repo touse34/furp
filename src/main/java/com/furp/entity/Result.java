a9cfd70025f5015f10cc80b23a2c6c6cee680582
touse34 <ssyyy0524@163.com>
2025-07-21 11:39:42 +0800

    加入时间戳

--- src/main/java/com/furp/entity/Result.java
+++ src/main/java/com/furp/entity/Result.java
@@ -3,6 +3,8 @@ package com.furp.entity;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 后端统一返回结果
@@ -10,29 +12,41 @@ import java.io.Serializable;
 @Data
 public class Result<T> implements Serializable{
 
-    private Integer code; //编码：1成功，0为失败
-    private String msg; //错误信息
+    private Integer code; //编码：200成功，其他为失败
+    private String msg; //响应信息
     private T data; //数据
+    private String timestamp; //时间戳
 
     public static <T> Result<T> success() {
         Result<T> result = new Result<>();
-        result.code = 1;
+        result.code = 200;
         result.msg = "success";
+        result.timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
         return result;
     }
 
     public static <T> Result<T> success(T object) {
         Result<T> result = new Result<>();
         result.data = object;
-        result.code = 1;
+        result.code = 200;
         result.msg = "success";
+        result.timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
         return result;
     }
 
     public static <T> Result<T> error(String msg) {
         Result<T> result = new Result<>();
         result.msg = msg;
-        result.code = 0;
+        result.code = 500;
+        result.timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+        return result;
+    }
+
+    public static <T> Result<T> error(Integer code, String msg) {
+        Result<T> result = new Result<>();
+        result.msg = msg;
+        result.code = code;
+        result.timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
         return result;
     }
 
