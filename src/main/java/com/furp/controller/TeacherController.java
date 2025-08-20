a3ed531337357a9ddd82011fefd570715f2182a4
Sirui <scysl14@nottingham.edu.cn>
2025-08-21 02:44:02 +0800

    PUT /api/v1/teacher/profile

--- src/main/java/com/furp/controller/TeacherController.java
+++ src/main/java/com/furp/controller/TeacherController.java
@@ -1,5 +1,6 @@
 package com.furp.controller;
 
+import com.furp.DTO.ReviewInfoVo;
 import com.furp.DTO.TeacherProfileDTO;
 import com.furp.DTO.TeacherResearchAreasResponseDTO;
 import com.furp.DTO.TeacherTimeSelectionDTO;
@@ -8,6 +9,7 @@ import com.furp.VO.TimeConfigVO;
 import com.furp.entity.Result;
 import com.furp.service.TeacherProfileService;
 import com.furp.service.TeacherResearchAreasResponseService;
+import com.furp.service.TeacherService;
 import com.furp.service.TimeSlotsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,8 @@ public class TeacherController {
     private TimeSlotsService timeSlotsService;
     @Autowired
     private TeacherProfileService teacherProfileService;
+    @Autowired
+    private TeacherService teacherService;
     @Autowired
     private TeacherResearchAreasResponseService teacherResearchAreasResponseService;
 
@@ -104,6 +108,21 @@ public class TeacherController {
         teacherProfileService.update(teacherProfileDTO);
         return Result.success();
     }
+
+    /**
+     * 4.1获取用户评审任务
+     * @param
+     * @return
+     */
+    @GetMapping("/v1/teacher/review-tasks")
+    public Result<List<ReviewInfoVo>> getReviewTasks(@RequestAttribute("teacherId") Integer teacherId) {
+        // 这里可以调用相应的服务方法获取评审任务
+        // 假设有一个方法 getReviewTasksByUserId(userId)
+        List<ReviewInfoVo> reviewInfo = teacherService.findReviewScheduleByTeacherId(teacherId);
+
+        return Result.success(reviewInfo);
+    }
+
 
     /**
      * 7.1获取教师研究领域
