3036e09c3e4b091d3da000a630d094166a6dc351
aziko <105570050+touse34@users.noreply.github.com>
2025-09-09 17:17:13 +0800

    Revert "加全局异常处理器 (#6)" (#7)
    This reverts commit 7987bb13fe8ad360284b61ff403308f20fe7e535.

--- src/main/java/com/furp/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.furp.exception;
-
-public class ResourceNotFoundException extends ServiceException {
-    public ResourceNotFoundException(String message) {
-        super(message, 404);
-    }
-}
--- src/main/java/com/furp/service/TeacherService.java
+++ src/main/java/com/furp/service/TeacherService.java
@@ -13,5 +13,5 @@ public interface TeacherService {
 
     public List<ReviewInfoVo> findReviewScheduleByTeacherId(Integer teacherId);
 
-    public void updateTaskStatus(Integer taskId, Integer currentTeacherId, StatusUpdateDTO dto);
+    public int updateTaskStatus(Integer taskId, Integer currentTeacherId, StatusUpdateDTO dto);
 }
